8dccde78b4f549d95761ef3cae22ff9a038a74f1
Fady Barsoum <fadybarsoum@users.noreply.github.com>
2022-05-27 15:02:39 -0700

    Delay text fade in to allow photos to be viewed

--- js/main.js
+++ js/main.js
@@ -167,13 +167,13 @@
 				setTimeout(function(){
 					$('.slider-text').removeClass('animated fadeInUp');
 					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
+				}, 1500);
 			},
 			before: function(){
 				setTimeout(function(){
 					$('.slider-text').removeClass('animated fadeInUp');
 					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
+				}, 1500);
 			}
 
 	  	});
